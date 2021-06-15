22832dfba326484081833341623eb4c6a9802499
bryant tunbutr <bryantt23@users.noreply.github.com>
2021-06-14 19:42:44 -0700

    Shows error when it's built and served

--- src/App.js
+++ src/App.js
@@ -1,36 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
-
-import { useState } from 'react';
+import Form from './Form';
+import { ErrorBoundary } from './ErrorBoundary';
 
 function App() {
-  const [query, setQuery] = useState('pikachu');
-  console.log('🚀 ~ file: App.js ~ line 8 ~ App ~ query', query);
-
-  const handleSubmit = async e => {
-    e.preventDefault();
-    console.log('🚀 ~ file: App.js ~ line 8 ~ App ~ query', query);
-
-    try {
-      const url = `https://pokeapi.co/api/v2/pokemon/${query}`;
-
-      const res = await fetch(url);
-      console.log('🚀 ~ file: App.js ~ line 18 ~ App ~ res', res);
-      const data = await res.json();
-      console.log('🚀 ~ file: App.js ~ line 20 ~ App ~ data', data);
-    } catch (error) {
-      console.log('🚀 ~ file: App.js ~ line 22 ~ App ~ error', error);
-    }
-  };
-
   return (
-    <div>
-      <form onSubmit={e => handleSubmit(e)}>
-        <input value={query} onChange={e => setQuery(e.target.value)}></input>
-
-        <input type='submit' value='Search' />
-      </form>
-    </div>
+    <ErrorBoundary>
+      <Form />
+    </ErrorBoundary>
   );
 }
 
---
+++ src/Form.js
@@ -0,0 +1,42 @@
+import { useState } from 'react';
+
+function Form() {
+  const [error, setError] = useState(null);
+  const [query, setQuery] = useState('pikachux');
+  console.log('🚀 ~ file: App.js ~ line 8 ~ App ~ query', query);
+
+  const handleSubmit = async e => {
+    e.preventDefault();
+    console.log('🚀 ~ file: App.js ~ line 8 ~ App ~ query', query);
+
+    try {
+      const url = `https://pokeapi.co/api/v2/pokemon/${query}`;
+
+      const res = await fetch(url);
+      //   console.log('🚀 ~ file: App.js ~ line 18 ~ App ~ res', res);
+      const data = await res.json();
+      console.log('🚀 ~ file: App.js ~ line 20 ~ App ~ data', data);
+    } catch (error) {
+      //   console.log('🚀 ~ file: App.js ~ line 22 ~ App ~ error', error);
+      setError(error);
+    }
+  };
+
+  if (error) {
+    // console.log('🚀 ~ file: Form.js ~ line 26 ~ Form ~ error', error);
+
+    throw new Error('I crashed! ' + error);
+    // throw new Error('I crashed!');
+  }
+  return (
+    <div>
+      <form onSubmit={e => handleSubmit(e)}>
+        <input value={query} onChange={e => setQuery(e.target.value)}></input>
+
+        <input type='submit' value='Search' />
+      </form>
+    </div>
+  );
+}
+
+export default Form;
