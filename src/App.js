c222490bb2339226e86d7ccb36bc98d298c3074d
bryant tunbutr <bryantt23@users.noreply.github.com>
2021-06-14 18:43:24 -0700

    Fetch pokemon

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
 
+import { useState } from 'react';
+
 function App() {
+  const [query, setQuery] = useState('pikachu');
+  console.log('🚀 ~ file: App.js ~ line 8 ~ App ~ query', query);
+
+  const handleSubmit = async e => {
+    e.preventDefault();
+    console.log('🚀 ~ file: App.js ~ line 8 ~ App ~ query', query);
+
+    try {
+      const url = `https://pokeapi.co/api/v2/pokemon/${query}`;
+
+      const res = await fetch(url);
+      console.log('🚀 ~ file: App.js ~ line 18 ~ App ~ res', res);
+      const data = await res.json();
+      console.log('🚀 ~ file: App.js ~ line 20 ~ App ~ data', data);
+    } catch (error) {
+      console.log('🚀 ~ file: App.js ~ line 22 ~ App ~ error', error);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form onSubmit={e => handleSubmit(e)}>
+        <input value={query} onChange={e => setQuery(e.target.value)}></input>
+
+        <input type='submit' value='Search' />
+      </form>
     </div>
   );
 }
