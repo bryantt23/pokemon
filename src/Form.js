71802cd5ccdf9155d77c59b0a48d07377a5228a1
bryant tunbutr <bryantt23@users.noreply.github.com>
2021-06-14 19:46:13 -0700

    Add not found message

--- src/Form.js
+++ src/Form.js
@@ -13,9 +13,13 @@ function Form() {
       const url = `https://pokeapi.co/api/v2/pokemon/${query}`;
 
       const res = await fetch(url);
-      //   console.log('🚀 ~ file: App.js ~ line 18 ~ App ~ res', res);
-      const data = await res.json();
-      console.log('🚀 ~ file: App.js ~ line 20 ~ App ~ data', data);
+      console.log('🚀 ~ file: App.js ~ line 18 ~ App ~ res', res);
+      if (!res.ok) {
+        setError('Not found');
+      } else {
+        const data = await res.json();
+        console.log('🚀 ~ file: App.js ~ line 20 ~ App ~ data', data);
+      }
     } catch (error) {
       //   console.log('🚀 ~ file: App.js ~ line 22 ~ App ~ error', error);
       setError(error);
